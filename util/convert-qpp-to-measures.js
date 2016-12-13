9746d78952e880e25dc87382ef3c41cc4d220916
Mari Miyachi <mari@navahq.com>
2016-12-13 11:15:56 -0800

    Add ACI measures data
    Closes #14
    
    Testing
    Validate new measures data by running cat versions/0.0.1/measures-data.json | node util/validate-data.js 0.0.1 measures and get output Valid!
    
    Reviewers: @gabesmed

--- util/convert-qpp-to-measures.js
+++ util/convert-qpp-to-measures.js
@@ -5,7 +5,7 @@
  * default to the latest.
  *
  * This script can be used as follows:
- * cat qpp_ia_measures.json | node convert-qpp-to-measures.js 0.0.1 > measures-data.json
+ * cat qpp_ia_measures.json | node convert-qpp-to-measures.js 0.0.1 ia > measures-data.json
  **/
 
 var fs = require('fs');
@@ -13,8 +13,7 @@ var path = require('path');
 
 var version = process.argv[2] || '0.0.1';
 
-// TODO (Mari): Only handles the conversion of Improvement Activity Measures
-var category = 'ia';
+var category = process.argv[3] || 'ia';
 
 var qpp = '';
 
@@ -32,9 +31,9 @@ process.stdin.on('end', () => {
 });
 
 /**
- * Takes a JSON cpiaActivities object and returns a measures data JSON object.
- * Replaces or adds key/value pairs from cpiaActivities as follows:
- *    cpiaActivity key | measuresData key
+ * Takes a QPP JSON object and returns a measures data JSON object.
+ * Replaces or adds key/value pairs from Improvement Activities as follows:
+ *    IA key | measuresData key
  *    ------------------------------------
  *    measure_title    | title
  *    measure_desc     | description
@@ -44,16 +43,40 @@ process.stdin.on('end', () => {
  *    N/A              | category
  *    N/A              | metricType (defaults to 'boolean')
  *    N/A              | firstPerformanceYear (defaults to the current year)
+ *
+ * For Advancing Care Information, the replacement is as follows:
+ *    ACI key               | measuresData key
+ *    ------------------------------------------------
+ *    measure_title                 | title
+ *    measure_desc                  | description
+ *    stage_name                    | measureSet
+ *    measure_id                    | measureId
+ *    base_score_required_sw        | isRequired
+ *    performance_score_weight_text | weight
+ *    measure_domain_desc           | objective
+ *    submitting_requirement_text   | metricType
+ *    bonus_optional_measure_sw     | isBonus
+ *    N/A                           | category
+ *    N/A                           | firstPerformanceYear (defaults to the current year)
  */
 function parseQpp(json) {
   var measureSetList = json.serviceData.categoryList;
-  var measureList = json.serviceData.cpiaActivities;
+  if (category === 'ia') {
+    var measureList = json.serviceData.cpiaActivities;
+  } else if (category === 'aci') {
+    var measureList = json.serviceData.aci_measures.aci_stage_name1_measures
+      .concat(json.serviceData.aci_measures.aci_stage_name2_measures);
+  }
   var result = [];
 
   for (var i=0; i< measureList.length; i++) {
     var measure = measureList[i];
     var obj = {};
     for (var j in measure) {
+      obj.category = category;
+      obj.metricType = 'boolean'; // default
+      obj.firstPerformanceYear = new Date().getFullYear();
+
       if (j === 'measure_title') {
         obj.title = measure[j];
       } else if (j === 'measure_desc') {
@@ -61,22 +84,63 @@ function parseQpp(json) {
       } else if (j === 'measure_id') {
         obj.measureId = measure[j];
       } else if (j === 'actvty_ctgry_desc') {
-        obj.subcategoryId = lowercaseFirstLetter(measure[j].replace('&','And')
-                                                           .replace(/ /g,'')
-                                                           .replace(/and([A-Z])/,"And$1"));
+        obj.subcategoryId = formatString(measure[j]);
       } else if (j === 'actvty_wghtng_cd') {
         obj.weight = measure[j].replace(/ /g, '').toLowerCase();
+      } else if (j === 'base_score_required_sw') {
+        obj.isRequired = measure[j] === 'No' ? false : true;
+      } else if (j === 'performance_score_weight_text') {
+        obj.weight = parseWeight(measure[j]);
+      } else if (j === 'measure_domain_desc') {
+        obj.objective = formatString(measure[j]);
+      } else if (j === 'submitting_requirement_text') {
+        obj.metricType = parseMetricType(measure[j]);
+      } else if (j === 'bonus_optional_measure_sw') {
+        obj.isBonus = measure[j];
+        if (obj.isBonus) {
+          obj.weight = 5; // override weight if measure is a bonus measure
+        }
       }
-
-      obj.category = category;
-      obj.metricType = 'boolean';
-      obj.firstPerformanceYear = new Date().getFullYear();
     }
     result.push(obj);
   }
   return JSON.stringify(result, null, 2);;
 }
 
-function lowercaseFirstLetter(string) {
+/**
+  * Removes spaces, replaces ampersands, capitalizes 'And' and 'Of' and
+  * lowercases the first letter.
+  */
+function formatString(string) {
+  string = string.replace('&','And')
+        .replace(/ /g,'')
+        .replace(/and([A-Z])/,"And$1")
+        .replace(/of([A-Z])/,"Of$1")
   return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+function parseWeight(weight) {
+  switch(weight) {
+    case '0':
+      return 0;
+    case '0 or 10%':
+      return 10;
+    case 'Up to 10%':
+      return 10;
+    case 'Up to 20%':
+      return 20;
+    default:
+      throw new Error("Invalid weight: " + weight);
+  }
+}
+
+function parseMetricType(metricType) {
+  switch(metricType) {
+    case 'Numerator\/ Denominator':
+      return 'proportion';
+    case 'Yes\/No Statement':
+      return 'boolean';
+    default:
+      throw new Error("Invalid metric type: " + metricType);
+  }
 }
