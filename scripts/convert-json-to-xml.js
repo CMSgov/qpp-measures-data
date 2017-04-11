42fdc6dc6255a7e9b59514d90f08d25daad5486e
Mari Miyachi <mari@navahq.com>
2017-04-11 14:30:56 -0700

    Sigularize measureSet and submissionMethod xml fields

--- scripts/convert-json-to-xml.js
+++ scripts/convert-json-to-xml.js
@@ -22,11 +22,20 @@ var json = '';
   * This is not valid XML syntax, so we use regex to replace these indices with
   * <measure></measure>. See this xml2js issue for details:
   * https://github.com/Leonidas-from-XIV/node-xml2js/issues/119
+  *
+  * The xml2js builder does not allow for tag name configuration, so we are
+  * using a fragile regex pattern to singularize array names. See here for
+  * details: https://github.com/Leonidas-from-XIV/node-xml2js#xml-builder-usage
   */
 function convertToXml(json) {
   var builder = new xml2js.Builder({rootName: schemaType});
   var xml = builder.buildObject(JSON.parse(json, 'utf8'));
-  process.stdout.write(xml.replace(/(<\/)?[0-9]{1,}(>)/g,'$1measure$2'));
+  process.stdout.write(
+    xml
+      .replace(/(<\/)?measureSets(>)/g,'$1measureSet$2')
+      .replace(/(<\/)?submissionMethods(>)/g,'$1submissionMethod$2')
+      .replace(/(<\/)?[0-9]{1,}(>)/g,'$1measure$2')
+  );
 }
 
 process.stdin.setEncoding('utf8');
