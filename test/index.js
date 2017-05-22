54488c0ce71bba9d02b9b09c099d7a638b30d40d
Aimee Barciauskas <aimee@navahq.com>
2017-05-22 17:36:16 -0400

    Add measures-schema-spec

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 require('./benchmarks/benchmarks-spec');
 require('./measures/measures-data-spec');
+require('./measures/measures-schema-spec');
 require('./util/is-inverse-benchmark-record-spec');
 require('./util/format-benchmark-record-spec');
---
+++ test/measures/measures-schema-spec.js
@@ -0,0 +1,48 @@
+const chai = require('chai');
+const assert = chai.assert;
+const exec = require('child_process').exec;
+const fs = require('fs');
+
+const testFilesFolder = 'test/examples/quality/';
+const qualityMeasureExampleFilenames = fs.readdirSync(testFilesFolder);
+const numExamples = qualityMeasureExampleFilenames.length;
+
+describe('measures schema', function() {
+  describe('measures-data.json', function() {
+    var capturedStdout = null;
+
+    before(function(done) {
+      var command = 'cat measures/measures-data.json | node scripts/validate-data.js measures';
+      exec(command, function(error, stdout, stderr) {
+        capturedStdout = stdout;
+        done();
+      });
+    });
+
+    it('are valid', function() {
+      assert.equal(capturedStdout, 'Valid!\n');
+    });
+  });
+
+  // TODO(aimee): Remove once obsolete. This will become obsolete once c2q measures are imported.
+  describe('example quality measures', function() {
+    var capturedStdoutExamples = [];
+
+    before(function(done) {
+      qualityMeasureExampleFilenames.forEach(function(qualityMeasureExampleFilename, idx) {
+        var command = 'cat ' + testFilesFolder + qualityMeasureExampleFilename + 
+          ' | node scripts/validate-data.js measures';
+        exec(command, function(error, stdout, stderr) {
+          capturedStdoutExamples.push(stdout);
+          if (idx === numExamples - 1) {
+            done();
+          }
+        });
+      });
+    });
+
+    it('are valid', function() {
+      assert.deepEqual(capturedStdoutExamples, Array(numExamples).fill('Valid!\n'));
+    });
+  });
+});
