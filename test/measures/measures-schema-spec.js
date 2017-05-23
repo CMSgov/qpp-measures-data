e09019340397e8a2c73157db8bc164c01bd8887c
Aimee Barciauskas <aimee@navahq.com>
2017-05-23 15:58:20 -0400

    QPPA-569: Add claims measures fields.
    https://jira.cms.gov/browse/QPPA-560
    
    Test plan: npm run test
    
    Reviewers: @marimiyachi

---
+++ test/measures/measures-schema-spec.js
@@ -0,0 +1,46 @@
+const chai = require('chai');
+const assert = chai.assert;
+const exec = require('child_process').exec;
+const fs = require('fs');
+
+const testFilesFolder = 'test/examples/quality/';
+const qualityMeasureExampleFilenames = fs.readdirSync(testFilesFolder);
+const numExamples = qualityMeasureExampleFilenames.length;
+
+describe('measures schema validates json', function() {
+  describe('measures-data.json', function() {
+    var capturedStdout = null;
+
+    before(function(done) {
+      var command = 'cat measures/measures-data.json | node scripts/validate-data.js measures';
+      exec(command, function(error, stdout, stderr) {
+        capturedStdout = stdout;
+        done();
+      });
+    });
+
+    it('is valid', function() {
+      assert.equal(capturedStdout, 'Valid!\n');
+    });
+  });
+
+  // TODO(aimee): Remove: This will become obsolete once c2q measures are imported into measures-data.
+  describe('example quality measures', function() {
+    qualityMeasureExampleFilenames.forEach(function(qualityMeasureExampleFilename) {
+      var capturedStdoutExamples = null;
+
+      before(function(done) {
+        var command = 'cat ' + testFilesFolder + qualityMeasureExampleFilename + 
+          ' | node scripts/validate-data.js measures';
+        exec(command, function(error, stdout, stderr) {
+          capturedStdoutExamples = stdout;
+          done();
+        });
+      });
+
+      it(qualityMeasureExampleFilename + ' is valid', function() {
+        assert.deepEqual(capturedStdoutExamples, 'Valid!\n');
+      });
+    });
+  });
+});
