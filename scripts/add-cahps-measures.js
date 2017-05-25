49e102c6f6ce4cf567d66984a558590c2e77b929
Aimee Barciauskas <aimee@navahq.com>
2017-05-25 13:14:24 -0400

    QPPA-498: Add cahps measures
    Closes #30
    
    https://jira.cms.gov/browse/QPPA-498
    
    Description:
    Adds a scripts/add-cahps-measures.js to import CAHPS measures from an origin CSV file.
    
    Test plan:
    `npm run test`
    
    Reviewers: @kencheeto @marimiyachi @samskeller

---
+++ scripts/add-cahps-measures.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+/**
+ *
+ * Script to add cahps measures to additional-measures.json from csv
+ * To run: `cat [DATA_CSV_FILE] | node scripts/add-cahps-measures.js`
+ * e.g. `cat cahps_measures_origin.csv | node scripts/add-cahps-measures.js`
+ */
+
+// Libraries
+const fs    = require('fs');
+const parse = require('csv-parse');
+const path  = require('path');
+
+const additionalMeasuresFilepath = '../util/additional-measures.json';
+var additionalMeasures = require(additionalMeasuresFilepath);
+const cahpsConsts = require('./cahps-consts');
+
+// Constants
+const CAHPS_CSV_COLUMNS = [
+  'Measure Name',
+  // The following columns are unused but prefer leaving it to be clear about
+  // the structure of the original import document.
+  'Measure Points',
+  'Measure Contribution'
+];
+
+// Initialize a string to store the CSV data.
+var cahpsMeasuresData = '';
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk !== null) {
+    cahpsMeasuresData += chunk;
+  }
+});
+
+function generateCahpsMeasure(record, idx) {
+  var measureTitle = record['Measure Name'];
+  var measureIdx = cahpsConsts.cahpsTitleToMeasureIdIndexMap[measureTitle];
+  if (measureIdx === undefined) {
+    throw 'No existing measure index matches title: "' + measureTitle + '"';
+  };
+  return {
+    category: 'quality',
+    firstPerformanceYear: 2017,
+    lastPerformanceYear: null,
+    metricType: 'cahps',
+    title: measureTitle,
+    description: '', // TBD: Will be provided by RAND,
+    nationalQualityCode: null,
+    measureType: 'patientEngagementExperience',
+    measureId: 'CAHPS_' + cahpsConsts.cahpsTitleToMeasureIdIndexMap[measureTitle],
+    eMeasureId: null,
+    nqfEMeasureId: null,
+    nqfId: cahpsConsts.nqfIdMap[measureTitle] || null,
+    isInverse: false,
+    strata: [],
+    isHighPriority: true,
+    primarySteward: 'Agency for Healthcare Research & Quality',
+    submissionMethods: [
+      'certifiedSurveyVendor'
+    ],
+    measureSets: [
+      'generalPracticeFamilyMedicine'
+    ]
+  };
+};
+
+process.stdin.on('end', function() {
+  parse(cahpsMeasuresData, {columns: CAHPS_CSV_COLUMNS, from: 2}, function(err, records) {
+    if (err) {
+      console.log(err);
+    } else {
+      // We want to overwrite CAHPS measures every time we run this script.
+      additionalMeasures = additionalMeasures.filter(function(measure) {
+        var re = /CAHPS_\d+/i;
+        return measure.measureId.match(re) === null;
+      });
+
+      records.forEach(function(record, idx) {
+        var measure = generateCahpsMeasure(record, idx);
+        if (measure) additionalMeasures.push(measure);
+      });
+
+      fs.writeFileSync(path.join(__dirname, additionalMeasuresFilepath), JSON.stringify(additionalMeasures, null, 2), 'utf8');
+    }
+  });
+});
--- test/measures/measures-data-spec.js
+++ test/measures/measures-data-spec.js
@@ -31,5 +31,41 @@ describe('measures data json', function() {
 
       assert.equal(intersection.size, multiPerformanceIds.size);
     });
+
+    describe('CAHPS measures', function() {
+      const cahpsMeasures = measuresData.filter(function(measure) {
+        return measure.measureId.match(/CAHPS_\d+/);
+      });
+
+      it('contains 12 correct CAHPS measures', function() {
+        const cahpsMeasures = measuresData.filter(measure => measure.measureId.match(/CAHPS_\d+/));
+        const commonCahpsProperties = {
+          'metricType': 'cahps',
+          'measureType': 'patientEngagementExperience',
+          'primarySteward': 'Agency for Healthcare Research & Quality',
+          'submissionMethods': ['certifiedSurveyVendor'],
+          'measureSets': ['generalPracticeFamilyMedicine'],
+          'firstPerformanceYear': 2017,
+          'category': 'quality',
+          'isHighPriority': true,
+          'isInverse': false
+        };
+        const nqfIdMap = {
+          'CAHPS for MIPS SSM: Getting Timely Care, Appointments and Information': '0005',
+          'CAHPS for MIPS SSM: How Well Providers Communicate': '0005',
+          'CAHPS for MIPS SSM: Patient\'s Rating of Provider': '0005',
+          'CAHPS for MIPS SSM: Courteous and Helpful Office Staff': '0005'
+        }
+        assert.equal(cahpsMeasures.length, 12);
+        cahpsMeasures.forEach(cahpsMeasure => {
+          assert.match(cahpsMeasure.title, /^CAHPS for MIPS/);
+          // these are the same for all CAHPS measures
+          assert.deepEqual(_.pick(cahpsMeasure, Object.keys(commonCahpsProperties)), commonCahpsProperties);
+          if (nqfIdMap[cahpsMeasure.title]) {
+            assert.equal(cahpsMeasure.nqfId, '0005');
+          }
+        });
+      });
+    });
   });
 });
