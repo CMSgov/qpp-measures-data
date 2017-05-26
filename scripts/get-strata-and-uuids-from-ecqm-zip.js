83b73ddae2788da5b94dbf71a3e34ccefabbf1e1
Kenshiro Nakagawa <kencheeto@gmail.com>
2017-05-25 19:03:57 -0700

    add some comments to script

--- scripts/get-strata-and-uuids-from-ecqm-zip.js
+++ scripts/get-strata-and-uuids-from-ecqm-zip.js
@@ -1,7 +1,26 @@
-// running this will generate a file in measures/quality-performace-rates.json
-// with the descriptions for each performance rate
-// claims pdfs can be downloaded from https://qpp.cms.gov/docs/QPP_quality_measure_specifications.zip
 // usage ./scripts/get-strata-and-uuids-from-ecqm-zip.js <path to zip of ecqm>
+// running this will generate a file at util/ecqm-strata.json
+// with an object like this for each ecqm:
+/*
+  {
+    "eMeasureId": "CMS117v5",
+    "eMeasureUuid": "40280381-52fc-3a32-0153-1a4ba57f0b8a",
+    "strata": [
+      {
+        "name": "strata1",
+        "description": "Children who have evidence showing they received recommended vaccines, had documented history of the illness, had a seropositive test result, or had an allergic reaction to the vaccine by their second birthday",
+        "eMeasureUuids": {
+          "initialPopulationUuid": "DA379EC2-EE2E-4548-AEF0-DD4F14F80279",
+          "denominatorUuid": "CC8AFFF0-A436-42CD-8322-5EBCEF9CBF06",
+          "numeratorUuid": "AE7A33AF-0DA7-4772-A23C-2D2CA732D53A",
+          "denominatorExclusionUuid": "19d26b5e-9be2-4313-80f4-67be1e0dde37",
+          "denominatorExceptionUuid": "01a1c883-e2e6-4aec-81de-17f3cd9b63b3"
+        }
+      }
+    ],
+    "metricType": "singlePerformanceRate"
+  },
+*/
 
 const _ = require('lodash');
 const fs = require('fs');
@@ -14,6 +33,60 @@ const zipPath = process.argv[2];
 if (!zipPath) {
   console.log('Missing required argument <path to zip>');
   process.exit(1);
+}
+
+/*
+return strata name, description, and uuids like so
+[
+  {
+    name: 'strata1',
+    description: 'Patients who initiated treatment within 14 days of the diagnosis',
+    eMeasureUuids: {
+      initialPopulationUuid: '25286925-4221-4396-9DE0-60EA606924DF',
+      denominatorUuid: 'CFB8E3E2-FF4F-4D25-B613-7EC142BAE8A9',
+      numeratorUuid: 'A399FA9C-48CF-41E5-812A-3445188B8301',
+      denominatorExclusionUuid: 'EEAD441F-B3B2-4DC9-A890-B35E14B38EA7',
+      denominatorExceptionUuid: 'E76F6606-1DC9-40DE-8A34-5B4B4E859152'
+    }
+  }
+  ...
+*/
+function extractStrata(measure) {
+  const details = measure.measure.measureDetails[0];
+  // our version of 'strata' are described as 'numerators'
+  // parse out strata descriptions from numerator text
+  const description = details.numeratorDescription[0];
+  // descriptions are like "Numerator 1: Patients who initiated treatment within 14 days of the diagnosis\nNumerator 2: Patients who initiated treatment and who had two or more additional services with an AOD diagnosis within 30 days of the initiation visit"
+  let strataDescriptions = _.compact(description.split('\n'))
+    // if multiple strata, they're enumerated as 'Numerator x: '
+    .filter(string => string.match(/^(Numerator \d: )/))
+    // all the text after 'Numerator 1:'
+    .map(string => string.substr('Numerator x: '.length).trim());
+  if (strataDescriptions.length === 0) {
+    // description stores single stratum otherwise
+    strataDescriptions = [description.trim()];
+  }
+
+  const strata = strataDescriptions.map((description, index) => ({
+    // we'll replace these manually to be contextually relevant
+    name: `strata${index + 1}`,
+    description
+  }));
+
+  // pull out uuids for each stratum
+  // ASSUMPTION: numerators are ordered the same as measure groupings
+  // if not we need to key on something
+  measure.measure.measureGrouping[0].group.forEach((group, index) => {
+    strata[index].eMeasureUuids = {
+      initialPopulationUuid: group.clause[0].$.uuid,
+      denominatorUuid: group.clause[1].$.uuid,
+      numeratorUuid: group.clause[2].$.uuid,
+      denominatorExclusionUuid: group.clause[3].$.uuid,
+      denominatorExceptionUuid: group.clause[5].$.uuid
+    }
+  });
+
+  return strata;
 }
 
 // gather list of xml files
@@ -34,46 +107,14 @@ const xmlFiles = fs.readdirSync(tmpDir).map(measureZip => {
 
 // parse files into JavaScript objects
 const promisifiedParseString = Promise.promisify(parseString);
-const promises = Promise.all(
+Promise.all(
   xmlFiles.map(xmlFile => {
     return promisifiedParseString(fs.readFileSync(path.join(tmpDir, '/xmls', xmlFile)))
       .then(xml => xml);
   })
-);
-
-function extractStrata(measure) {
-  // parse out strata descriptions from numerator text
-  const details = measure.measure.measureDetails[0];
-  const description = details.numeratorDescription[0];
-  // descriptions are like "Numerator 1: Patients who initiated treatment within 14 days of the diagnosis\nNumerator 2: Patients who initiated treatment and who had two or more additional services with an AOD diagnosis within 30 days of the initiation visit"
-  let strataDescriptions = _.compact(description.split('\n'))
-    .filter(string => string.match(/^(Numerator \d: )/))
-    .map(string => string.substr('Numerator x: '.length).trim());
-  if (strataDescriptions.length === 0) {
-    // single stratum
-    strataDescriptions = [description.trim()];
-  }
-
-  const strata = strataDescriptions.map((description, index) => ({
-    name: `strata${index + 1}`,
-    description
-  }));
-
-  measure.measure.measureGrouping[0].group.forEach((group, index) => {
-    strata[index].eMeasureUuids = {
-      initialPopulationUuid: group.clause[0].$.uuid,
-      denominatorUuid: group.clause[1].$.uuid,
-      numeratorUuid: group.clause[2].$.uuid,
-      denominatorExclusionUuid: group.clause[3].$.uuid,
-      denominatorExceptionUuid: group.clause[5].$.uuid
-    }
-  });
-
-  return strata;
-}
-
-// extract fields
-promises.then(measures => {
+)
+// extract data from converted JavaScript objects
+.then(measures => {
   return _.compact(measures.map(measure => {
     const details = measure.measure.measureDetails[0];
     const emeasureid = details.emeasureid[0];
@@ -96,7 +137,9 @@ promises.then(measures => {
       metricType: strata.length > 1 ? 'multiPerformanceRate' : 'singlePerformanceRate'
     };
   }));
-}).then(ecqms => {
+})
+// sort and write extracted data to disk
+.then(ecqms => {
   const sortedEcqms = _.sortBy(ecqms, ['eMeasureId']);
   fs.writeFileSync(path.join(__dirname, '../util/ecqm-strata.json'), JSON.stringify(sortedEcqms, null, 2));
 });
