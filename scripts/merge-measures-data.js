8632036b07d500d98a8fc6c4fc2d8a81f567669e
Kenshiro Nakagawa <kencheeto@gmail.com>
2017-05-26 00:48:35 -0700

    use guard clauses to un-nest ecqm script a little

--- scripts/merge-measures-data.js
+++ scripts/merge-measures-data.js
@@ -24,8 +24,6 @@ function mergeQpp(qppJson) {
   var performanceRatesJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../util/quality-performance-rates.json'), 'utf8'));
   // read in measures/quality-measures-additional-info.json
   var performanceRateAdditionalJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../util/quality-measures-strata-details.json'), 'utf8'));
-  // read in util/ecqm-strata.json
-  var ecqmStrataJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../util/ecqm-strata.json'), 'utf8'));
 
   var measuresNotFound = [];
   // iterate through all qppJson measures and find matching items from other json blobs
@@ -55,33 +53,33 @@ function mergeQpp(qppJson) {
 
   console.error('did not find measure details for the following', measuresNotFound);
 
+  // a separate pass for ecqm data to keep things simple
+  var ecqmStrataJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../util/ecqm-strata.json'), 'utf8'));
   qppJson.forEach(function(qppItem, index) {
-    if (qppItem.category === 'quality') {
-      // add data from ecqm xml specifications
-      const ecqmInfo = _.find(ecqmStrataJson, {'eMeasureId': qppItem.eMeasureId});
-      if (ecqmInfo) {
-        qppJson[index].eMeasureUuid = ecqmInfo.eMeasureUuid;
-        qppJson[index].metricType = ecqmInfo.metricType;
-        const oldStrata = qppJson[index].strata;
+    if (qppItem.category !== 'quality') return;
+    const ecqmInfo = _.find(ecqmStrataJson, {'eMeasureId': qppItem.eMeasureId});
+    if (!ecqmInfo) return;
 
-        // if none, just set it
-        if (oldStrata.length === 0) {
-          qppJson[index].strata = ecqmInfo.strata;
-        } else { // otherwise preserve name but override description, uuids
-          ecqmInfo.strata.forEach((newStratum, ecqmIndex) => {
-            if (oldStrata[ecqmIndex]) {
-              qppJson[index].strata[ecqmIndex].eMeasureUuids = newStratum.eMeasureUuids;
-              qppJson[index].strata[ecqmIndex].description = newStratum.description;
-            } else {
-              qppJson[index].strata[ecqmIndex] = newStratum;
-            }
-          });
-          if (oldStrata.length > 1) {
-            // ASSUMPTION: strata already available are in the same order as the ones extracted from the xml files
-            // @kencheeto: I think this is safe, since the ecqm strata fields already present in qppJson have at most one stratum
-            console.warn(qppItem.eMeasureId, ': we are not guaranteed the same ordering, please double check these strata values in the diff');
-          }
+    qppJson[index].eMeasureUuid = ecqmInfo.eMeasureUuid;
+    qppJson[index].metricType = ecqmInfo.metricType;
+    const oldStrata = qppJson[index].strata;
+
+    // if none, just set it
+    if (oldStrata.length === 0) {
+      qppJson[index].strata = ecqmInfo.strata;
+    } else { // only override description, uuids
+      ecqmInfo.strata.forEach((newStratum, ecqmIndex) => {
+        if (oldStrata[ecqmIndex]) {
+          qppJson[index].strata[ecqmIndex].eMeasureUuids = newStratum.eMeasureUuids;
+          qppJson[index].strata[ecqmIndex].description = newStratum.description;
+        } else {
+          qppJson[index].strata[ecqmIndex] = newStratum;
         }
+      });
+      if (oldStrata.length > 1) {
+        // ASSUMPTION: strata already available are in the same order as the ones extracted from the xml files
+        // @kencheeto: CMS156v5 is the only one that has more than one stratum and the ordering is correct
+        console.warn(qppItem.eMeasureId, ': we are not guaranteed the same ordering, please double check these strata values in the diff');
       }
     }
   });
