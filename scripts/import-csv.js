ab74a5cbc217008205e4a8415a719c9d7279bf53
Aimee Barciauskas <aimee@navahq.com>
2017-08-25 10:37:28 -0600

    Create boolean mappings for QCDR data

--- scripts/import-csv.js
+++ scripts/import-csv.js
@@ -5,13 +5,21 @@ const importCsv = function(measures, csvFile, config) {
   const records = parse(csvFile);
   const sourcedFields = config.sourced_fields;
   const constantFields = config.constant_fields;
+
   const newMeasures = records.map(function(record) {
     var newMeasure = {};
-    Object.entries(sourcedFields).forEach(function([measureKey, colIndex]) {
-      if (!record[colIndex]) {
-        throw TypeError('Column ' + colIndex + ' does not exist in source data');
+    Object.entries(sourcedFields).forEach(function([measureKey, columnObject]) {
+      if (typeof columnObject === 'number') {
+        if (!record[columnObject]) {
+          throw TypeError('Column ' + columnObject + ' does not exist in source data');
+        } else {
+          // measure data maps directly to data in csv
+          newMeasure[measureKey] = record[columnObject];
+        }
       } else {
-        newMeasure[measureKey] = record[colIndex];
+        // measure data requires mapping CSV data to new value, e.g. Y, N -> true, false
+        const mappedValue = columnObject.mappings[record[columnObject.index]];
+        newMeasure[measureKey] = mappedValue || columnObject.mappings['default'];
       }
     });
     Object.entries(constantFields).forEach(function([measureKey, measureValue]) {
