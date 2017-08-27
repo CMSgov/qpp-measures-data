c772d6aafa0ae47d2fd42a6a03dd3577ff25aecd
Aimee Barciauskas <aimee@navahq.com>
2017-08-27 14:05:06 -0600

    Add examples and fix header logic

--- scripts/measures/import-csv.js
+++ scripts/measures/import-csv.js
@@ -1,14 +1,21 @@
 const parse = require('csv-parse/lib/sync');
 const YAML = require('yamljs');
 
-// newmeasures=$(cat util/measures/2017-PIMMS-non-mips_measure_specifications.csv | node ./scripts/import-csv.js util/measures/qcdr-config.yaml)
-// echo $newmeasures | node scripts/validate-data.js measures
-// jq -s add <(cat util/measures/2017-PIMMS-non-mips_measure_specifications.csv | node ./scripts/import-csv.js util/measures/qcdr-config.yaml) <(cat measures/measures-data.json) | tee measures/measures-data.json
-const importCsv = require('./import-csv');
+/**
+ * import-csv reads a CSV file, takes a config file as an argument, reads
+ * CSV from stdin and outputs valid measures using convertCsvToMeasures.js
+ *
+ * example:
+ * $ cat util/measures/2017-PIMMS-non-mips_measure_specifications.csv | node ./scripts/measures/import-csv.js util/measures/qcdr-config.yaml
+ *
+ * test:
+ * $ cat util/measures/2017-PIMMS-non-mips_measure_specifications.csv | node ./scripts/measures/import-csv.js util/measures/qcdr-config.yaml | node scripts/validate-data.js measures 
+*/
+const convertCsvToMeasures = require('./convert-csv-to-measures');
 
 const config = YAML.load(process.argv[2]);
-// TODO: make this configurable
-const header = true;
+// TODO(aimee): test this
+const skipHeader = process.argv[3] && process.argv[3].split('=')[1] === 'false';
 let csvFile = '';
 
 process.stdin.setEncoding('utf8');
@@ -22,9 +29,9 @@ process.stdin.on('readable', function() {
 
 process.stdin.on('end', function() {
   const records = parse(csvFile, 'utf8');
-  if (header) {
+  if (!skipHeader) {
     records.shift();
   }
 
-  process.stdout.write(JSON.stringify(importCsv(records, config), null, 2));
+  process.stdout.write(JSON.stringify(convertCsvToMeasures(records, config), null, 2));
 });
