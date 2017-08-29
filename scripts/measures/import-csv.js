f933f17d65129a6b4c193a4f6f0664d34b99abad
Aimee Barciauskas <aimee@navahq.com>
2017-08-29 14:25:00 -0400

    Test w/o header and add example

--- scripts/measures/import-csv.js
+++ scripts/measures/import-csv.js
@@ -4,9 +4,12 @@ const YAML = require('yamljs');
 /**
  * import-csv reads a CSV file, takes a config file as an argument, reads
  * CSV from stdin and outputs valid measures using convertCsvToMeasures.js
+ * Assumes CSV has a header row.
  *
  * example:
  * $ cat util/measures/20170825-PIMMS-non-mips_measure_specifications.csv | node ./scripts/measures/import-csv.js util/measures/qcdr-config.yaml
+ * example for CSVs which have no header row:
+ * $ cat util/measures/20170825-PIMMS-non-mips_measure_specifications.csv | node ./scripts/measures/import-csv.js util/measures/qcdr-config.yaml --header=false
  *
  * test:
  * $ cat util/measures/20170825-PIMMS-non-mips_measure_specifications.csv | node ./scripts/measures/import-csv.js util/measures/qcdr-config.yaml | node scripts/validate-data.js measures
@@ -14,7 +17,6 @@ const YAML = require('yamljs');
 const convertCsvToMeasures = require('./convert-csv-to-measures');
 
 const config = YAML.load(process.argv[2]);
-// TODO(aimee): test this
 const skipHeader = process.argv[3] && process.argv[3].split('=')[1] === 'false';
 let csvFile = '';
 
