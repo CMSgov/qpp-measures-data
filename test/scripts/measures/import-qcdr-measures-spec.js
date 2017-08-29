e11d83758ca5de06e2d6d959eee0ee2fb80a93bc
Aimee Barciauskas <aimee@navahq.com>
2017-08-29 19:09:51 -0400

    Refactor

---
+++ test/scripts/measures/import-qcdr-measures-spec.js
@@ -0,0 +1,41 @@
+// Libraries
+const chai = require('chai');
+const assert = chai.assert;
+
+// Test data
+const testCsv = './test/scripts/measures/fixtures/test-qcdr.csv';
+const testCsv2Cols = './test/scripts/measures/fixtures/test-qcdr-2cols.csv';
+
+// Expected new measures
+const expectedMeasures = require('./fixtures/expected-measures.json');
+
+const { execSync } = require('child_process');
+
+const runTest = function(file) {
+  const cmd = 'cat ' + file + ' | node ./scripts/measures/import-qcdr-measures.js';
+  const measuresJson = execSync(cmd).toString();
+  return JSON.parse(measuresJson);
+};
+
+describe('convertCsvToMeasures', function() {
+  it('should create new measures', () => {
+    const newMeasures = runTest(testCsv);
+    assert.equal(newMeasures.length, 2);
+  });
+
+  it('should overwrite fields with the right csv data', () => {
+    const newMeasures = runTest(testCsv);
+    expectedMeasures.forEach(function(expectedMeasure, measureIdx) {
+      Object.entries(expectedMeasure).forEach(function([measureKey, measureValue]) {
+        assert.deepEqual(newMeasures[measureIdx][measureKey], measureValue);
+      });
+    });
+  });
+
+  it.only('throws an informative error when the column doesn\'t exist', function() {
+    const errorMessage = /Column 2 does not exist in source data/;
+    // assert.throws expects a function as its first parameter
+    const errFunc = () => { runTest(testCsv2Cols); };
+    assert.throws(errFunc, Error, errorMessage);
+  });
+});
