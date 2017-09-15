841586dc4d9ff16acabf94d13d598ea83f0a07eb
Rocky Kitamura <rockykitamura@gmail.com>
2017-09-14 20:55:29 -0700

    Enrich ACI relations

--- scripts/measures/enrich-measures-data.js
+++ scripts/measures/enrich-measures-data.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const fs = require('fs');
 
+const aciRelations = require('../../util/measures/aci-measure-relations.json');
 const cpcPlusGroups = require('../../util/measures/cpc+-measure-groups.json');
 
 var qpp = '';
@@ -19,10 +20,32 @@ process.stdin.on('end', () => {
 });
 
 function enrichMeasures(measures) {
+  enrichACIMeasures(measures);
   enrichCPCPlusMeasures(measures);
   return JSON.stringify(measures, null, 2);
 };
 
+/**
+ * Will add extra metadata to ACI measure that are not directly available
+ * in machine inferable format at https://qpp.cms.gov/api/v1/aci_measures
+ * After this function executes, an ACI measure will have reporting category and substitutes.
+ *  - substitutes: contains other measures that surrogates of a given measure.
+ *  - reportingCategory: corresponds to the measure performance category
+ * @param measures - the measures to enrich
+ */
+function enrichACIMeasures(measures) {
+  // add extra ACI metadata to ACI measure
+  measures
+    .filter(measure => measure.category === 'aci')
+    .forEach(measure => {
+      // find the relation and populate reporting category and substitutions
+      var aciRelation = aciRelations[measure.measureId];
+      if (aciRelation) {
+        measure.reportingCategory = aciRelation.reportingCategory;
+        measure.substitutes = aciRelation.substitutes;
+      }
+    });
+}
 
 /**
  * Will add extra metadata to CPC+ measures
