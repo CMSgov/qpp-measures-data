118259f8724e426e471a9b9c02cf8444d22b13f2
Roger Lam <me@lamroger.com>
2017-11-09 11:44:15 -0500

    lint

--- scripts/benchmarks/merge-benchmark-data.js
+++ scripts/benchmarks/merge-benchmark-data.js
@@ -1,36 +1,36 @@
 const BENCHMARK_LAYERS = [
-  "../../staging/benchmarks/benchmarks_base_2017.json",
-  "../../staging/benchmarks/benchmarks_updates_20171109.json"
+  '../../staging/benchmarks/benchmarks_base_2017.json',
+  '../../staging/benchmarks/benchmarks_updates_20171109.json'
 ];
 
 const UNIQUE_COLUMN_CONSTRAINT = [
-  "measureId",
-  "benchmarkYear",
-  "performanceYear",
-  "submissionMethod"
+  'measureId',
+  'benchmarkYear',
+  'performanceYear',
+  'submissionMethod'
 ];
 
-const get_benchmark_key = (benchmark) => {
-  let benchmark_key = "";
-  UNIQUE_COLUMN_CONSTRAINT.forEach((key_name) => {
-    benchmark_key += benchmark[key_name];
-  })
+const getBenchmarkKey = (benchmark) => {
+  let benchmarkKey = '';
+  UNIQUE_COLUMN_CONSTRAINT.forEach((keyName) => {
+    benchmarkKey += benchmark[keyName];
+  });
 
-  return benchmark_key
+  return benchmarkKey;
 };
 
-let merged_benchmarks = new Map();
+let mergedBenchmarks = new Map();
 
-BENCHMARK_LAYERS.forEach((benchmark_layer) => {
-  const benchmark_file = require(benchmark_layer);
-  benchmark_file.forEach((benchmark) => {
-    merged_benchmarks.set(get_benchmark_key(benchmark), benchmark);
+BENCHMARK_LAYERS.forEach((benchmarkLayer) => {
+  const benchmarkFile = require(benchmarkLayer);
+  benchmarkFile.forEach((benchmark) => {
+    mergedBenchmarks.set(getBenchmarkKey(benchmark), benchmark);
   });
 });
 
-let formatted_benchmarks = [];
-for (const value of merged_benchmarks.values()) {
-  formatted_benchmarks.push(value);
+let formattedBenchmarks = [];
+for (const value of mergedBenchmarks.values()) {
+  formattedBenchmarks.push(value);
 }
 
-process.stdout.write(JSON.stringify(formatted_benchmarks, null, 2));
+process.stdout.write(JSON.stringify(formattedBenchmarks, null, 2));
