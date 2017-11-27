6e5ac0c535b5b6425bc826cfc8ed71f9c687c770
Samuel Aquino <saquino@flexion.us>
2017-11-27 14:06:51 -0600

    Update regex to use .test. Update json generation to be more self documenting.

--- scripts/measures/archived/scripts/get-stratification.js
+++ scripts/measures/archived/scripts/get-stratification.js
@@ -4,24 +4,31 @@
 Extracts ecqm eMeasureIds and retrieves any measure stratifications
 Running this will generate a file at util/measures/additional-stratifications.json
 
-Usage: ./get-strata-and-uuids-from-ecqm-zip.js <path to zip of ecqm>
+Usage: ./get-stratification.js <path to zip of ecqm>
 
 most likely:
-./get-strata-and-uuids-from-ecqm-zip.js ecqm_eligibleclinician_jan2017.zip
+./get-stratification.js ecqm_eligibleclinician_jan2017.zip
 
 each generated ecqm stratification entry will look similar to this:
 
   {
-    "CMS137v5": {
-      "0BBF8596-4CFE-47F4-A0D7-9BEAB94BA4CD": [
-        "EFB5B088-CE10-43DE-ACCD-9913B7AC12A2",
-        "94B9555F-8700-45EF-B69F-433EBEDE8051"
-      ],
-      "7FFA49C4-D708-491E-85FE-6855F0A725DF": [
-        "ABC5631A-81C0-45C9-9306-716EAE39CDDB",
-        "2654804B-E6DA-4401-AA8B-1FEEACC0C259"
-      ]
-    }
+    "eMeasureId": "CMS137v5",
+    "mapping": [
+      {
+        "numeratorUuid": "0BBF8596-4CFE-47F4-A0D7-9BEAB94BA4CD",
+        "strata": [
+          "EFB5B088-CE10-43DE-ACCD-9913B7AC12A2",
+          "94B9555F-8700-45EF-B69F-433EBEDE8051"
+        ]
+      },
+      {
+        "numeratorUuid": "7FFA49C4-D708-491E-85FE-6855F0A725DF",
+        "strata": [
+          "ABC5631A-81C0-45C9-9306-716EAE39CDDB",
+          "2654804B-E6DA-4401-AA8B-1FEEACC0C259"
+        ]
+      }
+    ]
   }
 */
 
@@ -41,8 +48,8 @@ if (!zipPath) {
 }
 
 function extractStrata(measure) {
-  const returnValue = {};
-  const stratificationCodeType = 'SDE'
+  const returnValue = [];
+  const supplementDataType = 'SDE'
   measure.component.forEach((component, index) => {
     if (!component.populationCriteriaSection) {
       return;
@@ -51,17 +58,20 @@ function extractStrata(measure) {
     const components = component.populationCriteriaSection[0].component;
     const numeratorUuid = components.find(item => item.numeratorCriteria).numeratorCriteria[0].id[0].$.root;
     const stratList = [];
-    // loops through a Stratifier Criteria's components. If it's not a supplemental data component do another check to
-    // verify if it has the correct code for strata and add the content if it does.
+    // Loops through a Stratifier Criteria's components. If it's not a supplemental data component,
+    // add the stratum.
     components.filter(item => item.stratifierCriteria).forEach((component, index) => {
       const supplementalDataComponent = component.stratifierCriteria[0].component;
       if (_.isUndefined(supplementalDataComponent) ||
-        supplementalDataComponent[0].measureAttribute[0].code[0].$.code !== stratificationCodeType) {
+        supplementalDataComponent[0].measureAttribute[0].code[0].$.code !== supplementDataType) {
         stratList.push(component.stratifierCriteria[0].id[0].$.root);
       }
     });
     if (stratList.length !== 0) {
-      returnValue[numeratorUuid] = stratList;
+      const numeratorMap = {};
+      numeratorMap["numeratorUuid"] = numeratorUuid;
+      numeratorMap["strata"] = stratList;
+      returnValue.push(numeratorMap);
     }
   });
 
@@ -74,10 +84,10 @@ new AdmZip(zipPath).extractAllTo(tmpDir, true);
 // each measure has its own zip, collect name of SimpleXML files
 const xmlFiles = fs.readdirSync(tmpDir).map(measureZip => {
   const zip = new AdmZip(path.join(tmpDir, measureZip));
+  const MEASURE_XML_REGEX = (/([A-Z]{3})([0-9]{1,3})v([0-9])\.xml$/);
 
   const filename = zip.getEntries()
-    //.find(entry => entry.entryName.match(/([A-Z]{3})([0-9]{1,3})v([0-9])\.xml$/))
-    .find(entry => entry.entryName.match(/[0-9]\.xml$/))
+    .find(entry => MEASURE_XML_REGEX.test(entry.entryName))
     .entryName;
 
   // extract 'CMS75v5.xml' to /xmls
@@ -106,15 +116,12 @@ Promise.all(
       const eMeasureId = `CMS${measureId}v${version}`;
       return {
         eMeasureId,
-        strata
+        mapping: strata
       };
     }));
   })
 // map of measure id to stratification list
   .then(ecqms => {
-    const stratifications = {};
-    ecqms.forEach(ecqm => { stratifications[ecqm.eMeasureId] = ecqm.strata; });
-
     fs.writeFileSync(path.join(__dirname,
-      '../../../../util/measures/additional-stratifications.json'), JSON.stringify(stratifications, null, 2));
+      '../../../../util/measures/additional-stratifications.json'), JSON.stringify(ecqms, null, 2));
   });
