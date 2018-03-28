de2a8e7a2bfff7df015c78323a339d27f5ef11bd
Philip Dickinson <philip.jerome.dickinson@gmail.com>
2018-03-28 10:09:10 -0700

    Add behaviour for including claims-to-quality-related attributes to measures JSON (#108)
    * Initial work moving C2Q measures data processing into repo
    
    * Update claims-related measures parsing script to output only additional
    attributes.
    
    * Add other pycache dirs to ignore file.
    
    * Add claims-related attribute handling to data enrichment script.
    
    * Update comment on enriching claims-related attributes
    
    * Add readme for claims-related data parsing.
    
    * Update measures-data files to include new claims-related changes.
    
    * Update measures schema to account for new claims-related attributes.
    
    * Fix typo in claims-related measures readme.
    
    * Remove redundant args from spreadsheet parsing.
    
    * Update documentation URLs.
    
    * Add submissionMethods for every claims-related measure.
    
    * Update exception/exclusion attributes to match API naming.
    
    * Add prerequisites to claims-related measures readme.
    
    * Update spreadsheet parser to account for 2018 pages.
    
    * Add submission methods to eligibility options.
    
    * Update example measures to use new attribute names.

---
+++ claims-related/scripts/data_load.py
@@ -0,0 +1,88 @@
+"""Methods to load different data files into dataframes."""
+from data_load_helpers import format_column_title
+
+import pandas as pd
+
+
+def load_qpp_emeasure(excel_path):
+    """Load QPP eMeasure excel file into a DataFrame, updating some column names and values."""
+    # FIXME: Remove this functionality.
+    # Load the Excel file.
+    with open(excel_path, 'rb') as fname:
+        qpp_measures = pd.read_excel(
+            fname,
+            encoding=None,
+            skip_footer=21,
+            converters={'Measure #': int}
+        )
+    # Update column names.
+    qpp_measures.columns = qpp_measures.columns.map(format_column_title)
+    # There is one non-int value in the '# of Perf Rates' column.
+    # I will replace it by 1 because this value is 1 for Claims.
+    qpp_measures['of_perf_rates'] = qpp_measures['of_perf_rates'].replace(
+        'Claims/ Registry = 1\neCQM = 2', 1
+    ).astype(int, raise_on_error=False)
+
+    # There is one null line in the dataset. Drop it.
+    qpp_measures.dropna(how='all', inplace=True)
+
+    # I will split and dummify the reporting option column.
+    col_name = '2017_reporting_option_claims_registry_ehr'
+    for reporting_option in ['eHR', 'Registry', 'Claims']:
+        qpp_measures['if_' + reporting_option.lower()] = qpp_measures[col_name] \
+            .apply(lambda x: reporting_option in x)
+
+    # Clean up text columns in the dataframe.
+    columns = [
+        'emeasure_id',
+        'measure_title',
+        'claims_registry_measure_tag',
+        'emeasure_specific_measure_tag',
+        'additional_analytic_logic_or_guidance',
+        'inverse'
+    ]
+    for col in columns:
+        qpp_measures[col] = qpp_measures[col].str.strip()
+
+    # Format the measure numbers for consistency.
+    qpp_measures['measure'] = qpp_measures['measure'].apply(lambda x: '%0.2f' % x)
+    # Replace null values with 'NA'.
+    qpp_measures.fillna('NA', inplace=True)
+
+    return qpp_measures
+
+
+def load_single_source(excel_path):
+    """Load single source file into a DataFrame."""
+    column_dtypes = {
+        'age': 'str',
+        'code': 'str',
+        'coding_system': 'str',
+        'data_element_name': 'str',
+        'gender': 'str',
+        'measure': 'str',
+        'modifier': 'str',
+        'place_of_service': 'str',
+        'reporting_method': 'str'
+    }
+
+    with open(excel_path, 'rb') as fname:
+        single_source = pd.read_excel(fname, sheetname=-1, dtypes=column_dtypes)
+
+    # Update column names.
+    single_source.rename(columns=format_column_title, inplace=True)
+
+    # Format the measure codes to be compatible.
+    single_source['measure'] = single_source['measure'].apply(lambda x: "{:.2f}".format(x))
+
+    # Strip text in the dataframe.
+    for col in single_source.columns:
+        single_source[col] = single_source[col].dropna().astype('str').str.strip()
+
+    print('There are {} unique measures.'.format(single_source.measure.nunique()))
+    # Drop duplicates.
+    print('Shape before drop:', single_source.shape)
+    single_source.drop_duplicates(inplace=True)
+    print('Shape after drop:', single_source.shape)
+
+    return single_source
---
+++ claims-related/scripts/single_source_to_json.py
@@ -0,0 +1,109 @@
+"""
+Convert Single Source Excel Measure definitions into JSON.
+
+Inputs:
+    1. Single source Excel document containing quality measure definitions
+    for claims.
+
+    2. Top-level measures data for all QPP measures.
+    Source: https://github.com/CMSgov/qpp-measures-data/blob/master/measures/measures-data.json
+
+Outputs:
+    Single source JSON file combining both inputs into one large JSON object conforming to
+    this schema:
+        https://github.com/CMSgov/qpp-measures-data/blob/master/measures/measures-schema.yaml
+    It is built by supplementing the measures-data JSON object with the information for 74
+    claims-based quality measures in the single source Excel document.
+
+TODO: Clean up this code and add tests.
+"""
+import argparse
+import json
+
+from data_load import load_single_source
+
+from single_source_conversion_helpers import (
+    add_row_level_information_to_dataframe,
+    extract_eligibility_options_from_measure_dataframe,
+    merge_multiple_performance_options,
+    extract_performance_options_from_measure_dataframe,
+    merge_multiple_eligibility_options,
+)
+
+# Default filepaths for all required resources.
+DEFAULT_SINGLE_SOURCE_EXCEL_PATH = (
+    '/home/jovyan/work/data/2017_Claims_IndivMeasures_SingleSourceVersion2_11082017_andrea_edit'
+    '.xlsx'
+)
+DEFAULT_OUTPUT_JSON_PATH = '/home/jovyan/work/data/qpp-single-source.json'
+
+
+def _convert_measure_ids_to_match_nava_format(single_source_dict):
+    """Format measure number as 'measureId' string and add to measure dictionary."""
+    single_source_final = {}
+    for measure, measure_dict in single_source_dict.items():
+        measure_id = '{:03.0f}'.format(float(measure))
+        measure_dict.update({'measureId': measure_id})  # Include measureId in the JSON blob
+        single_source_final[measure_id] = measure_dict
+
+    return single_source_final
+
+
+def _main(**kwargs):
+    single_source_df = load_single_source(kwargs['single_source_filepath'])
+    single_source_df = add_row_level_information_to_dataframe(single_source_df)
+
+    single_source_dict = {}
+    # Assign eligibility and performance options for each measure.
+    for measure, measure_df in single_source_df.groupby('measure'):
+        single_source_dict[measure] = {
+            'eligibilityOptions': extract_eligibility_options_from_measure_dataframe(measure_df),
+            'performanceOptions': merge_multiple_performance_options(
+                extract_performance_options_from_measure_dataframe(measure_df)
+            )
+        }
+
+    # Merge eligibility options based on .00, .01, .02 measure IDs.
+    # As the measures have different measure IDs, this cannot be easily accomplished during the
+    # above iteration.
+    single_source_dict = merge_multiple_eligibility_options(single_source_dict)
+
+    # Update measureIds to match the format for later consolidation.
+    output = _convert_measure_ids_to_match_nava_format(single_source_dict)
+
+    # Write to JSON.
+    if kwargs.get('output_filepath', None):
+        with open(kwargs['output_filepath'], 'w') as f:
+            json.dump(output, f, sort_keys=True, indent=4)
+
+        print('The combined JSON file was written to {}.'.format(kwargs['output_filepath']))
+
+    return output
+
+
+def _get_arguments():
+    """Build argument parser."""
+    parser = argparse.ArgumentParser(
+        description='This script converts the single source Excel file to JSON.'
+    )
+
+    parser.add_argument(
+        '-i', '--single_source_filepath',
+        help='Single Source Excel file input filename',
+        required=False,
+        default=DEFAULT_SINGLE_SOURCE_EXCEL_PATH,
+        type=str)
+
+    parser.add_argument(
+        '-o', '--output_filepath',
+        help='Single source JSON output filepath',
+        required=False,
+        default=DEFAULT_OUTPUT_JSON_PATH,
+        type=str)
+
+    args = parser.parse_args()
+
+    return args.__dict__
+
+if __name__ == '__main__':
+    _main(**_get_arguments())
