f133ca3b44fed6fccbc27ba5c9f0dd457a9c2119
Sam Keller <sam@navahq.com>
2018-05-30 13:28:46 -0400

    Adding tests and modifying existing merge-measures ones to use new ia measures

---
+++ test/scripts/measures/2018/import-ia-measures-spec.js
@@ -0,0 +1,50 @@
+// Libraries
+const chai = require('chai');
+const assert = chai.assert;
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+
+// Test data
+const year = 2018;
+const testIAMeasuresCSV = '../../../test/scripts/measures/' + year + '/fixtures/test-ia-measures.csv';
+const outputArg = '../../../test/scripts/measures/' + year + '/fixtures/test-measures-data-output.json';
+const outputPath = path.join(__dirname, year.toString(), '../../' + outputArg);
+
+// Expected new measures
+const expectedMeasures = require('../' + year + '/fixtures/expected-ia-measures.json');
+
+// Function which executes script and reads in output file to a JS object.
+const runTest = (measuresCSV) => {
+  const cmd = `node ./scripts/measures/${year}/import-ia-measures.js ${measuresCSV} ${outputArg}`;
+  console.log(execSync(cmd, {stdio: 'pipe'}).toString());
+
+  const qpp = fs.readFileSync(outputPath, 'utf8');
+
+  return JSON.parse(qpp);
+};
+
+describe(year + ' import IA measures', () => {
+  it('should create new IA measures', () => {
+    const measures = runTest(testIAMeasuresCSV);
+    assert.equal(measures.length, 2);
+  });
+
+  it('should generate the expected IA measures json', () => {
+    const measures = runTest(testIAMeasuresCSV);
+    expectedMeasures.forEach((expectedMeasure, measureIdx) => {
+      _.each(expectedMeasure, (measureValue, measureKey) => {
+        assert.deepEqual(measures[measureIdx][measureKey], measureValue);
+      });
+    });
+  });
+});
+
+after(() => {
+  try {
+    fs.unlinkSync(outputPath);
+  } catch (err) {
+    console.log('Warning: ', outputPath, ' should have been deleted but was not.');
+  }
+});
