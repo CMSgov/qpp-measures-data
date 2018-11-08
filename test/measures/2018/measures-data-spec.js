f771dabd1a82132c01f0df711c12317ac9fa4d60
Sheena Bryant <sheena@navahq.com>
2018-11-08 16:10:00 -0500

    Remove .only

--- test/measures/2018/measures-data-spec.js
+++ test/measures/2018/measures-data-spec.js
@@ -28,7 +28,7 @@ describe(year + ' measures data json', function() {
       assert.deepEqual(measure.substitutes, ['PI_PHCDRR_2']);
     });
 
-    it.only('contains proper metadata', () => {
+    it('contains proper metadata', () => {
       const generated = {};
       measuresData
         .filter(m => m.category === 'pi')
