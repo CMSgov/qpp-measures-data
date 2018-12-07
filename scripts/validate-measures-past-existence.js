c2953f50a552a837e1441eb60b0feff02512d564
Kalvin <kalvinwang@users.noreply.github.com>
2018-12-06 17:58:16 -0800

    This PR validates the first performance year when building measures-data. For each measure with a first performance year prior to the current year, it checks the actual measures data for that previous year to confirm that the measure existed in that year.
    It also fixes a bug in the import-qcdr-measures script that had incorrectly assigned 2017 as the firstPerformanceYear for several dozen qcdr measures, instead of 2018 per the source CSV. The updated 2018 measures-data.json with fixed firstPerformanceYear values is included here.
    
    It also fixes the firstPerformanceYear for ACEP19 and ACEP20 in the source CSV, to 2018. Per @sarahnw:
    ```Based on old emails, they were left off some versions of the QCDR list and CMS opted not to include them since the issue wasn't brought to their attention until the submission window was open. Since they weren't reported against in 2017, I think we should have first performance year as 2018 since that is the first year they'll actually be allowed to be reported against.```
    
    The initial validation code in this PR was moved from [the Submissions API benchmarks calculation script](https://github.com/CMSgov/qpp-submissions-api/blob/10915baf3f0694bb1114c36a3813ee627763969a/tools/create-benchmarks/year3/calculate-deciles-lib.js) after the completion of https://github.com/CMSgov/qpp-submissions-api/pull/1564; it didn't make sense to do this measures validation on every benchmarks run. Instead, it should be done once here when the measures are generated, so problems can be caught early on.
    
    Jira: https://jira.cms.gov/browse/QPPA-2565
    
    Testing: Added unit tests. When excluding the qcdr bugfix, the validation script correctly output errors for all 100+ measures reported in the Jira ticket above, since they were misreported as existing in 2017. After the bugfix, the validation script correctly did not output any errors.
    
    This closes #224
    and completes #179
    
    Reviewers: @lamroger

---
+++ scripts/validate-measures-past-existence.js
@@ -0,0 +1,81 @@
+const _ = require('lodash');
+const QppMeasuresData = require('../');
+const Constants = require('../constants.js');
+
+const currentYear = Number(process.argv[2]);
+if (!Constants.validPerformanceYears.includes(currentYear)) {
+  throw Error(`${currentYear} is not a valid performance year; valid years are ${Constants.validPerformanceYears}`);
+}
+
+// The firstPerformanceYear field on measures isn't always accurate.
+// This function iterates through measures in a newly generated measures-data and
+// confirms that each measure actually existed in its firstPeformanceYear and subsequent years
+
+// The lastPerformanceYear field is always null since measures that
+// are no longer valid in a give performance year are removed entirely
+const validateExistenceOfCurrentYearMeasuresInPreviousYearMeasures = (currentYearMeasuresData) => {
+  // get an array of all first performance years, e.g. [2017, 2018], excluding the current year
+  const firstPerformanceYears =
+    _.pull(
+      _.uniq(_.map(currentYearMeasuresData, 'firstPerformanceYear')),
+      currentYear
+    );
+
+  if (!_.every(firstPerformanceYears, (year) => year < currentYear)) {
+    throw Error('One or more measures has a first performance year in the future');
+  }
+
+  firstPerformanceYears.forEach((previousYear) => {
+    // First performance year began on or before the previous year and
+    // last performance year isn't set yet or is greater or equal to the previous year
+    const isWithinValidYears = (measure) => {
+      return measure.firstPerformanceYear <= previousYear &&
+        (_.isNull(measure.lastPerformanceYear) || measure.lastPerformanceYear >= currentYear);
+    };
+
+    const previousYearMeasuresData = QppMeasuresData.getMeasuresData(previousYear);
+    // Get measures from the performance year measures-data with a
+    // performance year range that includes the previous year
+    const measuresSupposedlyExistingInPreviousYear = currentYearMeasuresData.filter(isWithinValidYears);
+
+    // Get measures from the actual previous year measures-data
+    // In theory this should match the measuresSupposedlyExistingInPreviousYear
+    // In practice these are examples of changes between 2017 and 2018:
+    // QUANTUM41 => Quantum41, MOA 1 => MOA1, ACI_INFBLO_1 => PI_INFBLO_1
+    const measuresExistingInPreviousYear = previousYearMeasuresData.filter(isWithinValidYears);
+    const previousYearMeasureIds = _.map(measuresExistingInPreviousYear, m => {
+      let measureId = m.measureId.toUpperCase();
+      if (previousYear === 2017) {
+        // ACI measures were all renamed to PI measures between 2017 and 2018
+        // and spaces were removed from all measures between 2017 and 2018
+        measureId = measureId.replace(/^ACI_/, 'PI_').replace(/ /, '');
+      }
+      return measureId;
+    });
+
+    // Print error message if a measure is valid in the performance year but is
+    // not in the previous year's measures data
+    measuresSupposedlyExistingInPreviousYear.forEach((measure) => {
+      const measureId = measure.measureId.toUpperCase();
+
+      if (!previousYearMeasureIds.includes(measureId)) {
+        console.log(`${measureId} in ${currentYear} measures data supposedly exists in ${previousYear} measures data but does not`);
+      }
+    });
+  });
+};
+
+let json = '';
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+  const chunk = this.read();
+  if (chunk !== null) {
+    json += chunk;
+  }
+});
+
+process.stdin.on('end', function() {
+  const newMeasuresData = JSON.parse(json);
+  validateExistenceOfCurrentYearMeasuresInPreviousYearMeasures(newMeasuresData);
+});
