c1018f3e2d62dc4ef667dc5c8b03eb83b82ac6b3
Samuel Aquino <saquino@flexion.us>
2019-07-23 16:05:58 -0500

    initial updates to measures

--- scripts/measures/2019/enrich-measures-data.js
+++ scripts/measures/2019/enrich-measures-data.js
@@ -48,19 +48,19 @@ function enrichMeasures(measures) {
  * generated-ecqm-data.json was made from running get-strata-uuids-from-ecqm-zip-2018.js on the eCQM_EP_EC_May2017.zip file
  */
 function mergeGeneratedEcqmData(measures) {
-  const generatedEcqmStrataJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../../../util/measures/generated-ecqm-data.json'), 'utf8'));
+  const generatedEcqmStrataJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../../../util/measures/' + currentYear + '/generated-ecqm-data.json'), 'utf8'));
 
   measures.forEach(function(qppItem, index) {
     if (qppItem.category !== 'quality') return;
     const ecqmInfo = _.find(generatedEcqmStrataJson, {'eMeasureId': qppItem.eMeasureId});
     if (!ecqmInfo) return;
-    ecqmInfo.strata.name = measures[index].strata.name;
+    if (ecqmInfo.strata.name) ecqmInfo.strata.name = measures[index].strata.name;
     measures[index].eMeasureUuid = ecqmInfo.eMeasureUuid;
     measures[index].metricType = ecqmInfo.metricType;
     measures[index].strata = ecqmInfo.strata;
   });
 
-  // This is a manually created file from from the eCQM_EP_EC_May2017.zip for the 4 missing measures.
+  // This is a manually created file from from the eCQM_EP_EC_May_CurrentYear.zip for the missing measures.
   const manuallyAddedEcqmStrataJson = JSON.parse(fs.readFileSync(path.join(__dirname, '../../../util/measures/' + currentYear + '/manually-created-missing-measures.json'), 'utf8'));
   measures.forEach(function(qppItem, index) {
     if (qppItem.category !== 'quality') return;
