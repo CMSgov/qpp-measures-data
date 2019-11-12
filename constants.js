99ab227021d742630983dbb1f360a2d117dff80e
shadford <37087269+shadford@users.noreply.github.com>
2019-11-12 14:12:33 -0500

    QPPSF-4894 - Calculate PY2020 Historic Quality Benchmarks (#296)
    * updated benchmarks for 2020
    
    * reverting 2019 benchmarks.csv
    
    * bumped measures version
    
    * fixed test failure in measures
    
    * cosmetic fixes
    
    * bumped version to 1.31.7

--- constants.js
+++ constants.js
@@ -1,9 +1,9 @@
 const Constants = {};
 
-Constants.validPerformanceYears = [2017, 2018, 2019];
+Constants.validPerformanceYears = [2017, 2018, 2019, 2020];
 
 // When you change the current performance year here, you must also change it in
 // /scripts/measures/build-measures
-Constants.currentPerformanceYear = 2019;
+Constants.currentPerformanceYear = 2020;
 
 module.exports = Constants;
