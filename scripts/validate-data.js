9117d775afae65adea7f5a12569cf05f1031c375
Sheldon Bachstein <bachsteinsk@gmail.com>
2019-12-12 09:45:27 -0500

    QPPA-3430 Update 2019 schema to error on extra fields (#306)
    * Update 2019 schema to error on extra fields
    
    * Refactor schema to use $merge
    
    DRYs up the schema
    
    * Update 2020 schema to be strict
    
    * Add specs for additional fields outside of schema
    
    Adds specs which test that schema validation fails if there are
    additional, unexpected data present in the json that is supposed to
    match the schema.
    
    This includes some refactoring of the scripts/validate-data.js file to
    make its logic more easily used in tests without shelling out.
    
    * 1.34.0
    
    * Add required fields
    
    * Remove unnecessary ajv references
    
    * Run `npm install`
    
    * Move isClinicalGuidelineChanges to quality
    
    Not needed in the base

--- scripts/validate-data.js
+++ scripts/validate-data.js
@@ -10,47 +10,48 @@
  * cat measures/measures-data.json | node scripts/validate-data.js measures 2018
  **/
 
-const Ajv = require('ajv');
 const path = require('path');
 const YAML = require('yamljs');
 
 const Constants = require('../constants.js');
-
-const ajv = new Ajv({ verbose: true });
-require('ajv-keywords')(ajv, 'uniqueItemProperties');
+const ValidateLib = require('./validate-json-data/validate-json-data-lib');
 
 const schemaType = process.argv[2];
 const performanceYear = (process.argv[3] || Constants.currentPerformanceYear).toString();
 
-let json = '';
-function validate(json) {
-  const valid = ajv.validate(
-    YAML.load(path.join(__dirname, '../' + schemaType,
-      performanceYear,
-      schemaType + '-schema.yaml')),
-    JSON.parse(json, 'utf8'));
+function validate(schema, json) {
+  const data = JSON.parse(json, 'utf8');
+
+  const {valid, errors, details} = ValidateLib.validate(schema, data);
+
   if (valid) {
-    console.log('Valid for ' + performanceYear + ' performance year schema');
+    console.log(`Valid for ${performanceYear} performance year schema`);
   } else {
-    console.log('Invalid for ' + performanceYear + ' performance year schema: ' +
-      ajv.errorsText(ajv.errors));
-    console.log('Detailed error: ', ajv.errors);
+    console.log(`Invalid for ${performanceYear} performance year schema: ${errors}`);
+    console.log('Detailed error: ', details);
   }
 }
 
-if (schemaType) {
-  process.stdin.setEncoding('utf8');
-
-  process.stdin.on('readable', function() {
-    const chunk = this.read();
-    if (chunk !== null) {
-      json += chunk;
-    }
-  });
-
-  process.stdin.on('end', function() {
-    validate(json);
-  });
-} else {
+if (!schemaType) {
   console.log('Please provide schema type, e.g. measures or benchmarks\nExample Command: cat ../measures/measures-data.json | node validate-data.js measures');
+  process.exit(1);
 }
+
+// Load the schema file
+const schema = YAML.load(path.join(__dirname, '../', schemaType, performanceYear, schemaType + '-schema.yaml'));
+
+// Read stdin into a string
+let json = '';
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', function() {
+  const chunk = this.read();
+  if (chunk !== null) {
+    json += chunk;
+  }
+});
+
+// Finally, validate the json against the schema
+process.stdin.on('end', function() {
+  validate(schema, json);
+});
