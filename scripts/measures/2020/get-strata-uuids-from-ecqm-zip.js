4a7aea6530d2498239512aade7bb4d9e2d5ff998
Samuel Aquino <saquino@flexion.us>
2020-05-29 16:40:29 -0500

    properly add all igrnored measured

--- scripts/measures/2020/get-strata-uuids-from-ecqm-zip.js
+++ scripts/measures/2020/get-strata-uuids-from-ecqm-zip.js
@@ -109,8 +109,7 @@ Promise.all(
       const measure = doc.QualityMeasureDocument;
       const emeasureid = measure.subjectOf[0].measureAttribute[0].value[0].$.value;
       console.log('EmeasureId = ' + emeasureid);
-      //const ignoredMeasureIds = ['135', '138', '144', '145', '156', '157', '347']
-      const ignoredMeasureIds = ['138', '144', '145', '156', '157', '347']
+      const ignoredMeasureIds = ['135', '138', '144', '145', '156', '157', '347']
       if (ignoredMeasureIds.includes(emeasureid)) {
         console.warn('WARNING: CMS' + emeasureid + ' has one numerator but multiple populations and needs to be added manually');
         return;
