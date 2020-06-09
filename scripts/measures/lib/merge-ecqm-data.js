10e390420bc5166e3a9163860011c045559c6338
Samuel Aquino <saquino@flexion.us>
2020-06-09 14:27:49 -0500

    strata naming issue where 2 stratum names are available but only one set of UUID's are made

--- scripts/measures/lib/merge-ecqm-data.js
+++ scripts/measures/lib/merge-ecqm-data.js
@@ -2,8 +2,16 @@ module.exports = (measures, generatedEcqms) => {
   generatedEcqms.forEach((generatedEcqm) => {
     const measure = measures.find(measure => measure.eMeasureId === generatedEcqm.eMeasureId);
     if (measure) {
-      if (measures.strata && measures.stata.name) {
-        generatedEcqm.strata.name = measures.strata.name;
+      if (measure.strata) {
+        measure.strata.forEach(function (strata, index) {
+          if (strata.name) {
+            if(generatedEcqm.strata[index] === undefined) {
+              generatedEcqm.strata.push({"name" : strata.name})
+            } else {
+              generatedEcqm.strata[index].name = strata.name;
+            }
+          }
+        })
       }
       measure.eMeasureUuid = generatedEcqm.eMeasureUuid;
       measure.metricType = generatedEcqm.metricType;
