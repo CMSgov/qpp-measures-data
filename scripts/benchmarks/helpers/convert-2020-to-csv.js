d7d5ed467afa131895dfcbb005e562e6c9ccf320
shad.wise <shad.wise@semanticbits.com>
2020-08-21 18:50:21 -0400

    updated source file to match attached spreadsheet

--- scripts/benchmarks/helpers/convert-2020-to-csv.js
+++ scripts/benchmarks/helpers/convert-2020-to-csv.js
@@ -7,7 +7,7 @@ const path = require('path');
 const fs = require('fs');
 
 const topline = 'Table 2: Historical MIPS Quality Measure Benchmark Results; created using PY2018 data and PY2020 Eligibility Rules,,,,,,,,,,,,,,,,,';
-const secline = '\n' + 'Measure_Name,Measure_ID,Collection_Type,Measure_Type,Benchmark,Standard_Deviation,Average,Decile_3,Decile_4,Decile_5,Decile_6,Decile_7,Decile_8,Decile_9,Decile_10,TOPPED_OUT,SevenPointCap,HighPriority';
+const secline = '\n' + 'Measure_Name,Measure_ID,Collection_Type,Measure_Type,Benchmark,Standard_Deviation,Average,Decile_3,Decile_4,Decile_5,Decile_6,Decile_7,Decile_8,Decile_9,Decile_10,Topped_Out,Seven_Point_Cap,High_Priority';
 const output = [];
 const convertedrecord = [];
 
@@ -38,41 +38,41 @@ parser.on('readable', function() {
 
   while (record = parser.read()) {
     // Remove any carriage returns in measure title
-    let measuretitle = record['Measure_Name'].replace(/[\n\r]+/g, '');
+    let measuretitle = record['Measure Title'].replace(/[\n\r]+/g, '');
 
     // Wrap in quotes, if any commas in measure title
-    if (record['Measure_Name'].indexOf(',') !== -1 && !record['Measure_Name'].startsWith('"') && !record['Measure_Name'].endsWith('"')) {
+    if (record['Measure Title'].indexOf(',') !== -1 && !record['Measure Title'].startsWith('"') && !record['Measure Title'].endsWith('"')) {
       measuretitle = '"' + measuretitle + '"';
     }
 
     // Wrap in quotes, if any quotes in measure title
-    if (record['Measure_Name'].charAt(0) === '"' && record['Measure_Name'].charAt(record['Measure_Name'].length - 1) === '"') {
+    if (record['Measure Title'].charAt(0) === '"' && record['Measure Title'].charAt(record['Measure Title'].length - 1) === '"') {
       measuretitle = measuretitle.replace(/^"/, '').replace(/"$/, '');
     }
 
-    if (record['Measure_Name'].indexOf('"') !== -1) {
+    if (record['Measure Title'].indexOf('"') !== -1) {
       measuretitle = '"' + measuretitle.replace(/"/g, '""') + '"';
     }
 
     convertedrecord.push(
       measuretitle,
-      record['Measure_ID'],
-      record['Collection_Type'].replace(/[\n\r]+/g, ''),
-      record['Measure_Type'].replace(/[\n\r]+/g, ''),
-      record['Benchmark'],
+      record['Measure ID'],
+      record['Collection Type'].replace(/[\n\r]+/g, ''),
+      record['Measure Type'].replace(/[\n\r]+/g, ''),
+      record['Measure has a Benchmark'],
       '',
-      record['Average'],
-      record['Decile_3'],
-      record['Decile_4'],
-      record['Decile_5'],
-      record['Decile_6'],
-      record['Decile_7'],
-      record['Decile_8'],
-      record['Decile_9'],
-      record['Decile_10'],
-      record['TOPPED_OUT'] === 'Y' ? 'Yes' : 'No',
-      record['SevenPointCap'] === 'Y' ? 'Yes' : 'No',
-      record['High_Priority'] === 'Y' ? 'Y' : 'N'
+      record['Average Performance Rate'],
+      record['Decile 3'],
+      record['Decile 4'],
+      record['Decile 5'],
+      record['Decile 6'],
+      record['Decile 7'],
+      record['Decile 8'],
+      record['Decile 9'],
+      record['Decile 10'],
+      record['Topped Out'] === 'Y' ? 'Yes' : 'No',
+      record['Seven Point Cap'] === 'Y' ? 'Yes' : 'No',
+      record['High Priority'] === 'Y' ? 'Y' : 'N'
     );
 
     output.push('\n' + convertedrecord.toString());
