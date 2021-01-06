da55b94fcb841b59b0b074c7ad4d1299b5119c0c
chetanmunegowda <chetan.munegowda@semanticbits.com>
2021-01-06 12:08:25 -0500

    QPPA-445: fix test coverage

--- test/scripts/measures/lib/merge-benchmark-metadata-spec.js
+++ test/scripts/measures/lib/merge-benchmark-metadata-spec.js
@@ -113,4 +113,46 @@ describe('merge-benchmark-metadata', () => {
     mergeBenchmarkMetadata(measures, metadata);
     expect(measures).to.deep.equal(expectedMeasures);
   });
+
+  it('should not merge benchmark metadata into base measure data', () => {
+    const measures = [
+      {measureId: '0', measureSpecification: {}},
+      {measureId: '1', measureSpecification: {}}
+    ];
+    const metadata = [
+      { measureId: '1',
+        claimsTruncated: 'N',
+        certifiedSurveyVendorTruncated: 'N',
+        electronicHealthRecordTruncated: 'N',
+        cmsWebInterfaceTruncated: 'N',
+        administrativeClaimsTruncated: 'N',
+        registryTruncated: 'N',
+        claimsSuppressed: 'N',
+        certifiedSurveyVendorSuppressed: 'N',
+        electronicHealthRecordSuppressed: 'Y',
+        cmsWebInterfaceSuppressed: 'Y',
+        administrativeClaimsSuppressed: 'N',
+        registrySuppressed: 'N',
+        claimsRemoved: 'Y',
+        certifiedSurveyVendorRemoved: 'N',
+        electronicHealthRecordRemoved: 'N',
+        cmsWebInterfaceRemoved: 'N',
+        administrativeClaimsRemoved: 'N',
+        registryRemoved: 'N',
+        claimsFlat: 'N',
+        certifiedSurveyVendorFlat: 'N',
+        electronicHealthRecordFlat: 'N',
+        cmsWebInterfaceFlat: 'N',
+        administrativeClaimsFlat: 'N',
+        registryFlat: 'N'
+      }
+    ];
+    const expectedMeasures = [
+      {measureId: '0', measureSpecification: {}},
+      {measureId: '1', measureSpecification: {}, isIcdImpacted: false, icdImpacted: [], isClinicalGuidelineChanged: true, clinicalGuidelineChanged: ['electronicHealthRecord', 'cmsWebInterface']}
+    ];
+
+    mergeBenchmarkMetadata(measures, metadata);
+    expect(measures).to.deep.equal(expectedMeasures);
+  });
 });
