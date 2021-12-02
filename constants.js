9030419f8e54bb1bfd886c6b606339ad5b9d6e92
removedporn <86824510+removedporn@users.noreply.github.com>
2021-12-02 21:04:56 +0800

    Delete constants.js

--- constants.js
+++
@@ -1,9 +0,0 @@
-const Constants = {};
-
-Constants.validPerformanceYears = [2017, 2018, 2019, 2020, 2021];
-
-// When you change the current performance year here, you must also change it in
-// /scripts/measures/build-measures
-Constants.currentPerformanceYear = 2021;
-
-module.exports = Constants;
