897ad09c21f023b186de069a3ef8bc52ee17971c
Kyle Apfel <kyle.apfel@semanticbits.com>
2022-05-11 18:09:21 -0500

    QPPSF-9970 - PY21 Performance Benchmarks

--- scripts/benchmarks/merge-benchmark-data.js
+++ scripts/benchmarks/merge-benchmark-data.js
@@ -1,4 +1,4 @@
-const { getOrderedFileNames, mergeBenchmarkFiles, validateUniqueConstraints } = require('./helpers/merge-benchmark-data-helpers.js');
+const { getOrderedFileNames, mergeBenchmarkLayers, validateUniqueConstraints } = require('./helpers/merge-benchmark-data-helpers.js');
 const path = require('path');
 
 const performanceYear = process.argv[2];
@@ -20,7 +20,7 @@ if (performanceYear) {
         return 0;
       }
     });
-  const formattedBenchmarks = mergeBenchmarkFiles(benchmarkLayerFiles, jsonDir);
+  const formattedBenchmarks = mergeBenchmarkLayers(benchmarkLayerFiles, jsonDir);
 
   validateUniqueConstraints(formattedBenchmarks);
 
