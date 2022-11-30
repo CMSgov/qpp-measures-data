159a31eaf821851909865541b4a7bf69d1a50ceb
ckawell-sb <104456459+ckawell-sb@users.noreply.github.com>
2022-11-30 15:51:51 -0600

    feat: QPPA-7169 misc upgrades

--- scripts/measures/lib/csv-json-converter.ts
+++ scripts/measures/lib/csv-json-converter.ts
@@ -29,7 +29,7 @@ import { InvalidValueError } from './errors';
 export function convertCsvToJson(csv: any) {
     const parsedCsv = prepareCsv(csv);
 
-    return parsedCsv.map((row: any) => {
+    let mappedCsv = parsedCsv.map((row: any) => {
         const measure = {};
         row['Category'] = row['Category'].toLowerCase().trim();
         let csvColumnNames;
@@ -56,6 +56,11 @@ export function convertCsvToJson(csv: any) {
 
         return measure;
     });
+
+    //remove any empty rows
+    _.remove(mappedCsv, _.isEmpty);
+    
+    return mappedCsv;
 }
 
 function mapInput(columnName: string, csvRow: any, category: string) {
@@ -177,7 +182,7 @@ function csvFieldToBoolean(field: string, value: string): boolean {
 
 function prepareCsv(csv: any): any {
     //parse csv.
-    const parsedCsv: Object[] = parse(csv, { columns: true });
+    const parsedCsv: Object[] = parse(csv, { columns: true, relax_column_count: true });
     
     //trim keys in parsed csv.
     for (let i = 0; i < parsedCsv.length; i++) {
