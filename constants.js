5caa42706131b111d46871cb9bc59c3315ca1f3d
JC5H <laura.jackson@semanticbits.com>
2023-01-05 16:37:04 -0500

    QPPA-7254 Add 2023 as valid PY; add deciles 1 and 2

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const Constants = {};
 
-Constants.validPerformanceYears = [2017, 2018, 2019, 2020, 2021, 2022];
+Constants.validPerformanceYears = [2017, 2018, 2019, 2020, 2021, 2022, 2023];
 
 // When you change the current performance year here, you must also change it in
 // /scripts/measures/build-measures
