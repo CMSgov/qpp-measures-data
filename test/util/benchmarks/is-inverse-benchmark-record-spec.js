9c5c2e9a47d4fa5caea11efb1b0cdf4332ec36d0
ckawell-sb <104456459+ckawell-sb@users.noreply.github.com>
2023-08-11 12:12:18 -0500

    feat: QPPA-7860 fix test

--- test/util/benchmarks/is-inverse-benchmark-record-spec.js
+++ test/util/benchmarks/is-inverse-benchmark-record-spec.js
@@ -2,7 +2,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 // Utils
-const isInverseBenchmarkRecord = require('./is-inverse-benchmark-record');
+const isInverseBenchmarkRecord = require('../../../scripts/benchmarks/archive/is-inverse-benchmark-record');
 
 describe('isInverseBenchmarkRecord', function() {
   it('should default to false', function() {
