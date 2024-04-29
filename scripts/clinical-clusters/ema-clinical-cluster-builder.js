d92125b6243ea84af7c1bd92909f31d0a81bac0b
Sivakumar Srinivasulu <siva.srinivasulu@semanticbits.com>
2024-04-29 17:15:13 -0500

    added script to read ppt file and extract clinical data

--- scripts/clinical-clusters/ema-clinical-cluster-builder.js
+++ scripts/clinical-clusters/ema-clinical-cluster-builder.js
@@ -22,7 +22,7 @@ const _ = require('lodash');
 const parse = require('csv-parse/sync').parse;
 
 const MAX_SPECIALITY_SET_SIZE = 6;
-const SUPPORTED_PERFORMANCE_YEARS = [2017, 2018, 2019, 2020, 2021, 2022, 2023];
+const SUPPORTED_PERFORMANCE_YEARS = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
 
 let measuresJson = '';
 const performanceYear = parseInt(process.argv[2], 10);
@@ -62,6 +62,10 @@ const specialSpecialtySetRelations = {
     registry: []
   },
   2023: {
+    claims: [],
+    registry: []
+  },
+  2024: {
     claims: [],
     registry: []
   }
@@ -161,6 +165,10 @@ const specialClusterRelations = {
     registry: []
   },
   2023: {
+    claims: [],
+    registry: []
+  },
+  2024: {
     claims: [],
     registry: []
   }
