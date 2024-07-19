42b0fe47d5b93ccce02776ef8db82bb1343440b6
ckawell-sb <ckawell1@gmail.com>
2024-07-19 11:56:17 -0500

    feat: QPPA-0000 fix init script

---
+++ scripts/mvp/2025/update-mvp.ts
@@ -0,0 +1,31 @@
+import fs from 'fs';
+
+import * as index from '../../../index';
+import {
+  info,
+  warning
+} from '../../logger';
+
+export function updateMvps(performanceYear: string) {
+  const filePath = `mvp/${performanceYear}/mvp-enriched.json`;
+
+  // Delete the existing file
+  try {
+    fs.unlinkSync(filePath);
+    info(`File ${filePath} deleted.`);
+  } catch (err: any) {
+    if (err.code === "ENOENT") {
+      warning(`File ${filePath} not found; proceeding to next step...`);
+    } else {
+      throw err;
+    }
+  }
+
+  index.createMVPDataFile(performanceYear);
+  index.updateProgramNames(performanceYear);
+}
+
+/* c8 ignore next */
+if (process.argv[2] && process.argv[2] !== "--coverage")
+  /* c8 ignore next */
+  updateMvps(process.argv[2]);
