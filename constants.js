a98ae4714e93496ed9ef5c9f6c6efee9170ffcd9
Chetan Munegowda <chetan.munegowda@semanticbits.com>
2024-07-19 12:48:41 -0400

    QPPA-9185: initialize py25 measures

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const Constants = {};
 
-Constants.validPerformanceYears = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
+Constants.validPerformanceYears = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025];
 
 // When you change the current performance year here, you must also change it in
 // /scripts/measures/build-measures
