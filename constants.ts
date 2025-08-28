7017588c4716510a699af34f95d05a0aebb53c14
ckawell-sb <ckawell1@gmail.com>
2025-08-28 14:19:25 -0500

    feat: QPPA-10773 Documentation upgrades

--- constants.ts
+++ constants.ts
@@ -8,8 +8,8 @@ interface IConstants {
 }
 
 export const Constants: IConstants = {
-  validPerformanceYears: [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025],
-  currentPerformanceYear: 2022,
+  validPerformanceYears: [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025, 2026],
+  currentPerformanceYear: 2025,
   mvpMeasuresHelper: [
     {
       measureIdKey: 'qualityMeasureIds',
